0e771789fc3ef556ec794aca1caadc21f60d67bb
LilithElina <sarah.pohl@outlook.de>
2015-11-20 13:08:50 +0100

    Corrected typos and some code

--- PlotData_ttest.R
+++ PlotData_ttest.R
@@ -15,7 +15,7 @@ colnames(data.long)[2:3] <- c("AA", "Conc")
 
 #### Plotting the data ####
 # install.packages("ggplot2")  # uncomment if you dont have ggplot2 installed
-#require(ggplot2)
+# require(ggplot2)
 ## Additional comment from Sarah: You shouldn't actually use require() like this
 ## and here is why: http://yihui.name/en/2014/07/library-vs-require/
 ## Basically, require() is (or should be) only used inside functions.
@@ -37,15 +37,15 @@ ggplot(data.long, aes(x=response, y=Conc, color=AA)) +
 
 ## optimization of graph:
 ## graphs use same x/y scales and data is not always in the same range
-## for each AA. Free scales ephasize the differences in each AA
+## for each AA. Free scales emphasize the differences in each AA
 ggplot(data.long, aes(x=response, y=Conc, color=AA)) +
   geom_boxplot() + facet_wrap(~AA, scales = "free")
-## With scales = "free" each facet gets its own x-axis lables. this is
-## unnecessary because x-axis labels/limits are the same accross all plots
+## With scales = "free" each facet gets its own x-axis labels. this is
+## unnecessary because x-axis labels/limits are the same across all plots.
 ## use "free_y" instead.
 ggplot(data.long, aes(x=response, y=Conc, color=AA)) +
   geom_boxplot() + facet_wrap(~AA, scales = "free_y")
-## facets look crowded, it may be helpful to limit the numer of facet columns
+## facets look crowded, it may be helpful to limit the numer of facet columns.
 ## limit facets to 4 columns for better presentation of the data
 ggplot(data.long, aes(x=response, y=Conc, color=AA)) +
   geom_boxplot() + facet_wrap(~AA, scales = "free_y", ncol = 4)
@@ -54,12 +54,12 @@ ggplot(data.long, aes(x=response, y=Conc, color=AA)) +
 ## statistical testing example with t.test
 t.test(Conc~response, data.long, mu=0, alt = "two.sided", 
        conf=0.95, Var.eq=F, paired=F)
-## this tests for differences in AA Concentration in responder and
+## this tests for differences in AA concentration in responder and
 ## non-responder group but does not differentiate between the AA.
 ## We need a solution that tests for each AA between R and N groups.
 
 ## Use the split function to split data for individual AA
-## return data ist a list of data frames, 1 df for each AA.
+## return data is a list of data frames, 1 df for each AA.
 split.data <- split(data.long,data.long$AA)
 
 ## to have a look at the data
@@ -82,22 +82,24 @@ simpleTTfunc <- function(x) {
 ## test the function on the extracted data
 simpleTTfunc(aa1)
 
-## use lapply (list-apply) the function to all elements of the split.data list
-lapply(list, simpleTTfunc)
+## use lapply (list-apply) to apply the function to all elements of the
+## split.data list
+lapply(split.data, simpleTTfunc)
 
 ## this approach can be elaborated:
 otherTTfunc <- function(x){
   p <- t.test(x$Conc~x$response)$p.value  # run the t test and return only the p-value
   m <- tapply(x$Conc, x$response, mean)   # calculate the mean concentration per group
-  s <- tapply(x$Conc, x$response, sd)
+  s <- tapply(x$Conc, x$response, sd)     # calculate the standard deviation
   # return the results
-  c(AA=AA[1], m[1], s[1], m[2], s[2], pval=p)  # you do not need an explicit return() statement in R
-  }
+  c(mean=m[1], sd=s[1],
+    mean=m[2], sd=s[2], pval=p)  # you do not need an explicit return() statement in R
+}
 
 ## run function on data per amino acid
 ## lapply returns a list!
 tmp <- lapply(split.data, otherTTfunc)
 
 ## to convert the list into a useable data frame we use 
-## rbind() to bind all elements to da dataframe
+## rbind() to bind all elements to a dataframe
 data.result <- do.call(rbind,tmp)
