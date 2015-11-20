307be55ecaa99f27139d4597608e910cbefd2ce3
Peter Braubach <braubach.peter@mh-hannover.de>
2015-11-20 11:58:27 +0100

    Commented install.packages - do not trigger package installations and/or updates on other peoples system!. Some commenting and cleaning of code and one additional example. Will rework when example data is posted. -p

--- PlotData_ttest.R
+++ PlotData_ttest.R
@@ -1,17 +1,21 @@
-#how to make ggplot
-#import data and convert it in long format & for 
-#converting it into long format you need "reshape2" package
-install.packages("reshape2")
+############################################################
+# MHH & Twincore R Club                                    #
+# 2015-11-18                                               #
+# Problem presented by Haroon                              #
+############################################################
+
+#### import data ####
+## import data and convert it in long format & for 
+## converting it into long format you need "reshape2" package
+# install.packages("reshape2")  # uncomment if you dont have reshape2 installed
 library(reshape2)
-sample.data = read.table("sample.data.csv", header=TRUE, sep=",")
-data.long=melt(sample.data, c("response"))
-colnames(data.long)[2:3]=c("AA", "Conc")
+sample.data <- read.table("sample.data.csv", header=TRUE, sep=",")
+data.long   <- melt(sample.data, c("response"))
+colnames(data.long)[2:3] <- c("AA", "Conc")
 
-# for ggplot
-
-install.packages("ggplot2")
+#### Plotting the data ####
+# install.packages("ggplot2")  # uncomment if you dont have ggplot2 installed
 #require(ggplot2)
-
 ## Additional comment from Sarah: You shouldn't actually use require() like this
 ## and here is why: http://yihui.name/en/2014/07/library-vs-require/
 ## Basically, require() is (or should be) only used inside functions.
@@ -30,46 +34,70 @@ ggplot(data.long, aes(x=response, y=Conc, color=AA)) +
 ggplot(data.long, aes(x=response, y=Conc, color=AA)) +
   geom_boxplot() + facet_wrap(~AA)
 
-# to make the graph scale free
+
+## optimization of graph:
+## graphs use same x/y scales and data is not always in the same range
+## for each AA. Free scales ephasize the differences in each AA
 ggplot(data.long, aes(x=response, y=Conc, color=AA)) +
   geom_boxplot() + facet_wrap(~AA, scales = "free")
-# free only y axis
+## With scales = "free" each facet gets its own x-axis lables. this is
+## unnecessary because x-axis labels/limits are the same accross all plots
+## use "free_y" instead.
 ggplot(data.long, aes(x=response, y=Conc, color=AA)) +
   geom_boxplot() + facet_wrap(~AA, scales = "free_y")
-# for better visualization
+## facets look crowded, it may be helpful to limit the numer of facet columns
+## limit facets to 4 columns for better presentation of the data
 ggplot(data.long, aes(x=response, y=Conc, color=AA)) +
   geom_boxplot() + facet_wrap(~AA, scales = "free_y", ncol = 4)
 
 
-# to apply t.test
-
+## statistical testing example with t.test
 t.test(Conc~response, data.long, mu=0, alt = "two.sided", 
        conf=0.95, Var.eq=F, paired=F)
-# but we need t.test for individual Amino acids b/t R and N groups
+## this tests for differences in AA Concentration in responder and
+## non-responder group but does not differentiate between the AA.
+## We need a solution that tests for each AA between R and N groups.
 
-# apply split function to split data for individual AA in both groups
+## Use the split function to split data for individual AA
+## return data ist a list of data frames, 1 df for each AA.
+split.data <- split(data.long,data.long$AA)
 
-split(data.long, "AA")
-split.data=split(data.long,data.long$AA)
-
-#to have a look on first AA
-x = split.data[[1]]
-# have a look on other AA
+## to have a look at the data
+## be carefult to use [[ as operator for list indexing 
+## as [ would return a list and not the object stored at this list
+## position.
+str(split.data)
+split.data[[1]]
 split.data[[2]]
 split.data[[3]]
 
-# function to do a t test and calculate the group means
-ttfunct = function(x){
-  # run the t test and return only the p-value
-  p=t.test(x$Conc~x$response)$p.value
-  # calculate the mean concentration per response
-  m=tapply(x$Conc, x$response, mean)
+## extract the first df to a new object (aa1) for further development
+aa1 <- split.data[[1]]
+
+#### solve the problem in general and apply the solution to all parts
+## of the problem
+simpleTTfunc <- function(x) {
+  t.test(x$Conc ~ x$response)
+}
+## test the function on the extracted data
+simpleTTfunc(aa1)
+
+## use lapply (list-apply) the function to all elements of the split.data list
+lapply(list, simpleTTfunc)
+
+## this approach can be elaborated:
+otherTTfunc = function(x){
+  p <- t.test(x$Conc~x$response)$p.value  # run the t test and return only the p-value
+  m <- tapply(x$Conc, x$response, mean)   # calculate the mean concentration per group
+  s <- tapply(x$Conc, x$response, sd)
   # return the results
-  return(c(m,p))
+  c(AA=AA[1], m[1], s[1], m[2], s[2], pval=p)  # you do not need an explicit return() statement in R
   }
 
-# run function on data per amino acid
-tmp = lapply(split.data, FUN = ttfunct)
-# convert the result to data frame
-data.result = do.call(rbind,tmp)
-colnames(data.result) <- c("N.mean", "R.mean", "p.value")
+## run function on data per amino acid
+## lapply returns a list!
+tmp <- lapply(split.data, otherTTfunc)
+
+## to convert the list into a useable data frame we use 
+## rbind() to bind all elements to da dataframe
+data.result <- do.call(rbind,tmp)
